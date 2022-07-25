71337d18bd55a73e42ce8af0a08d82839da03de0
YongGuk Kim <kyk3531@gmail.com>
2022-07-25 23:27:48 +0900

    22/07/15

---
+++ 집합과 맵/백준 10815번.js
@@ -0,0 +1,19 @@
+const input = require("fs")
+  .readFileSync("example.txt")
+  .toString()
+  .trim()
+  .split("\n"); //'/dev/stdin'
+
+const [N, M] = [+input[0], +input[2]];
+const [hasCards, cards] = [
+  new Set(input[1].split(" ").map(Number)),
+  input[3].split(" ").map(Number),
+];
+
+let answer = "";
+for (let i = 0; i < M; i++) {
+  if (hasCards.has(cards[i])) answer += 1 + " ";
+  else answer += 0 + " ";
+}
+
+console.log(answer);
