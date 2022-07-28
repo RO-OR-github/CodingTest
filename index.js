57bd2e18d1b42e38fd0bed608a094a903646f8fb
YongGuk Kim <kyk3531@gmail.com>
2022-07-28 23:28:50 +0900

    22/07/28

--- index.js
+++ index.js
@@ -1,47 +1,32 @@
-const input = require("fs").readFileSync("example.txt").toString().split("\n"); //'/dev/stdin'
+const array = require("fs").readFileSync("example.txt").toString().split("\n");
 
-const om = Number(input.shift());
+const stack = [];
+const result = [];
 
-const ea = [];
-const we = [];
-const so = [];
-const no = [];
-let [maxew, maxsn, minew, minsn] = [0, 0, 0, 0];
-let width = 0;
-for (i = 0; i < input.length; i++) {
-  let dir = input[i].split(" ");
-  switch (Number(dir[0])) {
-    case 1:
-      ea.push(Number(dir[1]));
+const len = array.shift();
+
+for (let i = 0; i < len; i++) {
+  switch (array[i]) {
+    case "pop":
+      result.push(stack.pop() || -1);
       break;
-    case 2:
-      we.push(Number(dir[1]));
+
+    case "size":
+      result.push(stack.length);
       break;
-    case 3:
-      so.push(Number(dir[1]));
+
+    case "empty":
+      result.push(stack[0] ? 0 : 1);
       break;
-    case 4:
-      no.push(Number(dir[1]));
+
+    case "top":
+      result.push(stack[stack.length - 1] || -1);
       break;
+
     default:
-      console.log("잘못된값");
+      stack.push(array[i].split(" ")[1]);
       break;
   }
 }
-if (ea.length == 2) {
-  if (so.length == 2) {
-  } else {
-  }
-} else {
-  if (so.length == 2) {
-  } else {
-  }
-}
 
-// maxew = Math.max(...eawe);
-// maxsn = Math.max(...sono);
-// minew = Math.min(...eawe);
-// minsn = Math.min(...sono);
-
-// width = maxew * maxsn - minew * minsn;
-console.log(width * om);
+console.log(result.join("\n"));
